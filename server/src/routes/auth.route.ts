a1899ea79a8a63656370b7ff4542d044ad88297a
Kong Lee <chicongxq2610@gmail.com>
2023-05-26 10:43:22 +0700

    refactor: config

--- server/src/routes/auth.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { signinOrSignup } from '../controllers/auth.controller';
-
-const authRouter = Router();
-
-authRouter.post('/signinOrSignup', signinOrSignup);
-
-export default authRouter;
